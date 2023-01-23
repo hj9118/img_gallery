28f4d477755c5b2e55ce6f5f82f8365ce9864902
Jiho Heo <104545906+hj9118@users.noreply.github.com>
2023-01-23 18:16:22 +0900

    Vanilla JS version

---
+++ script.js
@@ -0,0 +1,62 @@
+let req = new XMLHttpRequest();
+req.open('GET', './json/image_list.json');
+req.onreadystatechange = function () {
+  if (this.readyState == 4) {
+    let data = JSON.parse(this.response);
+    for (var i = 0; i < data.length; i++) {
+      let div = document.createElement('div');
+      div.setAttribute('class', 'image');
+      div.onclick = function () {
+        this.classList.toggle('image-selected');
+      };
+      div.onmouseover = function () {
+        let element = this;
+        this.timerId = setTimeout(function () {
+          element.classList.add('image-magnified');
+        }, 1000);
+        div.onmouseover = function () {
+          clearTimeout(this.timerId);
+          this.classList.remove('image-magnified');
+        };
+      };
+      let img = document.createElement('img');
+      img.src = data[i];
+      div.appendChild(img);
+      document.body.appendChild(div);
+    }
+  }
+};
+
+req.send();
+
+function selectAll(btn) {
+  let images = document.getElementsByClassName('image');
+  for (var i = 0; i < images.length; i++) {
+    if (btn.value == 'Unselect All') {
+      images[i].classList.remove('image-selected');
+    } else {
+      images[i].classList.add('image-selected');
+    }
+  }
+  if (btn.value == 'Unselect All') {
+    btn.value = 'Select All';
+  } else {
+    btn.value = 'Unselect All';
+  }
+}
+
+function slideshow(btn) {
+  let images = document.getElementsByClassName('image');
+  let index = 0;
+  images[index].classList.add('image-magnified');
+
+  let intervalId = setInterval(function () {
+    images[index].classList.remove('image-magnified');
+    index++;
+    if (index < images.length) {
+      images[index].classList.add('image-magnified');
+    } else {
+      clearInterval(intervalId);
+    }
+  }, 1000);
+}
